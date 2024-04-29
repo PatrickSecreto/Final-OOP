328a5f7b9aacab7755b35a1e8c49211ab3dfe6ab
Patrick Secreto <22-07061@g.batstate-u.edu.ph>
2024-04-30 03:35:20 +0800

    try

--- CircuitsForDummies.java
+++ CircuitsForDummies.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.circuitsfordummies;
 
-/**
- *
- * @author ACER-NITRO
- */
 public class CircuitsForDummies {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
