fe1b354c670967d844b35acdaf76a53354f585f6
Patrick Secreto <22-07061@g.batstate-u.edu.ph>
2024-04-30 01:48:02 +0800

    first commit - committing all files to the repository

---
+++ CircuitsForDummies.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.circuitsfordummies;
+
+/**
+ *
+ * @author ACER-NITRO
+ */
+public class CircuitsForDummies {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
