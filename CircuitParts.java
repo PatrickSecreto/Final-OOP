57c2a9e27e64c014ad7298e2344bde4773f60ba5
Patrick Secreto <22-07061@g.batstate-u.edu.ph>
2024-05-01 17:13:17 +0800

    added few necessary codes to run

--- CircuitParts.java
+++ CircuitParts.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.circuitsfordummies;
 
-/**
- *
- * @author ACER-NITRO
- */
+import java.util.Scanner;
+
 public class CircuitParts {
-    
+    Scanner scanner = new Scanner(System.in);
+        
+    public void circuitPartsChoice(){
+        int choice = 0;
+        
+        while (choice != 7){
+            System.out.println("\nWhat part of circuits do you want to learn?:\n"
+                    + "1. Breadboard \n" 
+                    + "2. Resistors\n" 
+                    + "3. Capacitors\n" 
+                    + "4. Transistors (PNP, NPN)\n" 
+                    + "5. Diodes (LED, Sensors, etc.)\n" 
+                    + "6. Jumper Wires\n" 
+                    + "7. Back");
+
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("description abt breadboard");
+                    break;
+                case 2:
+                    System.out.println("description abt resistors");
+                    break;
+                case 3:
+                    System.out.println("description abt capacitors");
+                    break;
+                case 4:
+                    System.out.println("description abt transistors");
+                    break;
+                case 5:
+                    System.out.println("description abt diodes");
+                    break;
+                case 6:
+                    System.out.println("description abt jumper wires");
+                    break;
+                case 7:
+                    System.out.println("exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select from the selection above.");
+            }
+        }
+    }
 }
--- ParallelDesc.java
+++ ParallelDesc.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.circuitsfordummies;
 
-/**
- *
- * @author ACER-NITRO
- */
+import java.util.Scanner;
+
 public class ParallelDesc {
+    Scanner scanner = new Scanner(System.in);
     
+    public void parallelDescChoice(){
+        int choice = 0;
+        
+        while (choice != 5){
+            System.out.println("\nCircuit design for Parallel Circuits:");
+            System.out.println(" ---------------------------------\n"
+                             + " |        |           |          |\n"
+                             + " |        |           |          |\n"
+                             + "___       |           |          |\n"
+                             + " _       { }         { }        { }\n"
+                             + "___      { }         { }        { }\n"
+                             + " _        |           |          |\n"
+                             + " |        |           |          |\n"
+                             + " |        |           |          |\n"
+                             + " ---------------------------------");
+
+            System.out.println("\nWhat do you want to learn about parallel circuits?\n"
+                    + "1. How to calculate Voltage?\n" 
+                    + "2. How to calculate Current?\n" 
+                    + "3. How to calculate Resistance of each resistor?\n"
+                    + "4. Calculator for Parallel Circuits\n"
+                    + "5. Back");
+            System.out.print("\nEnter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Formula for voltage in parallel circuits:");
+                    break;
+                case 2:
+                    System.out.println("Formula for current in parallel circuits:");
+                    break;
+                case 3:
+                    System.out.println("Formula for resistance in parallel circuits:");
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select from the selection above.");
+                    break;
+            }
+        }
+    }
 }
